55ed4d484e083ae4b7cacea93501deeb33965b4e
junes231 <deyongw3@gmail.com>
2025-08-16 11:06:24 +0800

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -10,12 +10,12 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyA8rJiJnyB6QHgkesFekaRy7f0oftXaF0c',
-  authDomain: 'funnel-editor-netlify.firebaseapp.com',
-  projectId: 'funnel-editor-netlify',
-  storageBucket: 'funnel-editor-netlify.firebasestorage.app',
-  messagingSenderId: '498506838505',
-  appId: '1:498506838505:web:95f20fdfbb260c2b271b78',
+  apiKey: "AIzaSyAbh_OpiB1RddfEFkQFGQ-66oNrdi9FLZY",
+  authDomain: "funnel-editor-demo.firebaseapp.com",
+  projectId: "funnel-editor-demo",
+  storageBucket: "funnel-editor-demo.firebasestorage.app",
+  messagingSenderId: "304895960294",
+  appId: "1:304895960294:web:967e2af5dcc0b6cf2e4727"
   measurementId: 'G-RVRL76REP7',
 };
 
